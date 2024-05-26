34c33d5c3f13d46e0804d047370be9e918c7322d
Hira Ejaz Ahmed Shaikh <167759551+hiraahmedshaikh@users.noreply.github.com>
2024-05-26 14:32:12 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# for addition
+def addition (x,y):
+    return x+y
+
+# for substraction
+def substraction (x,y):
+    return x-y
+
+# for multiplication
+def multiply(a, b):
+    return a * b
+
+# for remainder
+def remainder(a, b):
+    return a % b
+
+# for cube
+def cube(a):
+    return a ** 3
+
+# for square
+def square(a):
+    return a ** 2
+
